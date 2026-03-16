412eb0df98497dea2579c1afe4c62edce16a2693
Ricardo Shiro <ricardohaikawa@gmail.com>
2026-03-16 00:09:35 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,13 @@ public class Program {
 		Seller newSeller = new Seller( null, "Greg","greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted!  New Id = " + newSeller.getId());
+	
+		System.out.println("\n === TEST 5 : Seller update! === ");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!!");
 	}
+	
 
 }
--- src/model/dao/Impl/SellerDaoJDBC.java
+++ src/model/dao/Impl/SellerDaoJDBC.java
@@ -28,47 +28,64 @@ public class SellerDaoJDBC implements SellerDao {
 	public void insert(Seller obj) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement( 
-	
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					+ Statement.RETURN_GENERATED_KEYS);
-			
+			st = conn.prepareStatement(
+
+					"INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES "
+							+ "(?, ?, ?, ?, ?)",
+					+Statement.RETURN_GENERATED_KEYS);
+
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartmen().getId());
-			
+
 			int rowsAffected = st.executeUpdate();
-			
-				if (rowsAffected > 0) {
-					ResultSet rs = st.getGeneratedKeys();
-					if (rs.next()) {
-						int id = rs.getInt(1);
-						obj.setId(id);
-					}
-					DB.closeResultSet(rs);
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
 				}
-				else {
-					throw new DbException("Unexpected error! No rows affected!!");
-				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!!");
 			}
-				catch (SQLException e){
-					throw new DbException(e.getMessage());
-				
-				}	
-		finally {
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally {
 			DB.closeStatement(st);
 		}
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
 
+					"UPDATE seller " + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+							+ " WHERE Id = ? ");
+
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartmen().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+
+		}
+
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
